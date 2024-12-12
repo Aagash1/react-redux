fd59cdb5c949fb5e7d1baefce56340bb41adef58
Aagash1 <aagashsethupathi.ad20@bitsathy.ac.in>
2024-12-12 18:25:24 +0530

    redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,6 @@
-// Counter Actions
 export const increment = () => ({ type: 'INCREMENT' });
 export const decrement = () => ({ type: 'DECREMENT' });
 export const reset = () => ({ type: 'RESET' });
 
-// Notes Actions
 export const addNote = (note) => ({ type: 'ADD_NOTE', payload: note });
 export const deleteNote = (index) => ({ type: 'DELETE_NOTE', payload: index });
